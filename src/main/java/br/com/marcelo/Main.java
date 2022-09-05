41791001d6f2015aa14908f59edba6bbc7f814ce
hgrafa <hugorafell@gmail.com>
2022-09-05 09:28:28 -0300

    Adding tests to the Main projects

--- src/main/java/br/com/marcelo/Main.java
+++ src/main/java/br/com/marcelo/Main.java
@@ -1,19 +1,31 @@
 package br.com.marcelo;
 
+import br.com.marcelo.entities.AluguelCarro;
 import br.com.marcelo.entities.Carro;
+import br.com.marcelo.services.AluguelCarroService;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate dataFrabricacao = LocalDate.now();
+        LocalDate dataFrabricacao = LocalDate.parse("2018-01-01");
+        LocalDateTime inicio = LocalDateTime.of(2022, 02, 10, 15, 30);
+        LocalDateTime fim = LocalDateTime.of(2022, 02, 10, 16, 40);
 
-        System.out.println(dataFrabricacao);
 
         Carro car1 = new Carro("Joao", "Ford", dataFrabricacao);
 
-        System.out.println(car1);
+        AluguelCarro aluguel = new AluguelCarro(inicio, fim, car1);
+
+        System.out.println(aluguel);
+
+        AluguelCarroService servicoDeAluguel = new AluguelCarroService(50.0, 230.0);
+
+        servicoDeAluguel.processaNotaFiscal(aluguel);
+
+        System.out.println(aluguel);
 
     }
 }
